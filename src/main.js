c11583bcc29544b296ea0aa00d026140c9d84638
Filipe Câncio <filipe.cancio@outlook.com>
2020-01-29 11:33:50 -0300

    Evolução em .gitignore

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 function main() {
   var plan = planilha('1AMwWCGbRUGUFCo73lGp-FgyyjIq7Wyljvyjjd0jLw0w');
-  plan.setValor('pagina01',[['Código','Nome','Idade'],1,2,3,4,[5,'Rosana',30],6,7,8,9],'A1');
+  plan.setValor('pagina01','Coisa rápida','A1');
   return 'ok';
 }
